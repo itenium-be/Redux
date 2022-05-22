96fcc2d309b2ef6f77cc2b9947211f85f1470eab
Wouter Van Schandevijl <woutervs@hotmail.com>
2022-05-22 19:34:16 +0200

    Keep State Minimal and Derive Additional Values + Use Selector Functions to Read from Store State

--- angular-ngrx/src/app/shared/store/store.ts
+++ angular-ngrx/src/app/shared/store/store.ts
@@ -3,21 +3,21 @@ import { createSelector } from 'reselect';
 
 import * as providers from './reducers/providers.reducer';
 import * as providerDetails from './reducers/providerDetails.reducer';
-import * as activeProviders from './reducers/activeProviders.reducer';
 
 
 /** GLOBAL Store State */
 export interface GlobalState {
   providers: providers.ProvidersState;
   providerDetails: providerDetails.ProviderDetailsState;
-  activeProviders: activeProviders.ActiveProvidersState;
 }
 
 export const reducers: ActionReducerMap<GlobalState> = {
   providers: providers.reducer,
   providerDetails : providerDetails.reducer,
-  activeProviders: activeProviders.reducer,
 };
+
+
+export const getActiveProviders = (state: GlobalState) => state.providers.providers.filter(x => x.active);
 
 
 
